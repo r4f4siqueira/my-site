0a3a8f67529dc68f9e7b0efaf61c01355f1f6787
r4f4siqueira <r4f4siqueira@gmail.com>
2025-06-13 16:29:37 -0400

    ✨ Componente `LinksRedesSociais`

--- src/app/home/components/LinksRedesSociais.tsx
+++ src/app/home/components/LinksRedesSociais.tsx
@@ -1,9 +1,27 @@
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { MdOutlineEmail } from 'react-icons/md';
+
+const links = [
+    { nome: 'Linkedin', titulo: 'Rafael Siqueira', icon: <FaLinkedin />, link: '', target: '' },
+    { nome: 'GitHub', titulo: 'r4f4siqueira', icon: <FaGithub />, link: '', target: '' },
+    { nome: 'Facebook', titulo: 'Rafael Siqueira', icon: <FaFacebook />, link: '', target: '' },
+    { nome: 'Instagran', titulo: '@r4f4siqueira', icon: <FaInstagram />, link: '', target: '' },
+    { nome: 'Gmail', titulo: 'r4f4siqueira@gmail.com', icon: <MdOutlineEmail />, link: '', target: '' },
+];
 
 export default function LinksRedesSociais() {
-    return (
-        <Box>
-            <Button>Analize</Button>
+    return links.map((item) => (
+        <Box
+            key={item.nome}
+            display={'flex'}
+            flexDirection={'row'}
+            alignItems={'center'}
+            fontSize={32}
+            justifyContent={'space-between'}
+        >
+            {item.icon}
+            <Text>{item.titulo}</Text>
         </Box>
-    );
+    ));
 }

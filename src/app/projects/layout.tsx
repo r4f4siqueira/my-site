9fc2a0d2733cf5a6c3f68bfefb2494d33a092e05
r4f4siqueira <r4f4siqueira@gmail.com>
2026-06-26 16:00:41 -0400

    ✨ New Version site 🔥 I removed unnecessary tools and redesigned the site according to modern standards.

--- src/app/projects/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-import ProjectsContext from './projectsContext';
-
-export default function ProjectsLayout({ children }: { children: ReactNode }) {
-    return <ProjectsContext>{children}</ProjectsContext>;
-}
